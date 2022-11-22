051ebe4bb02a6b595088693f1d9a6b7de7a3586e
rva-mike <mike.levy.rva@gmail.com>
2022-11-21 21:21:52 -0500

    create-react-app, navbar component, styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-title">Site Name</a>
+            <ul>
+                <li>
+                    <a href="/pricing">Pricing</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
