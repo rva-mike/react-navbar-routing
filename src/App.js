b77d475d6969596b14c5fd6e5cb7e7da1a771832
rva-mike <mike.levy.rva@gmail.com>
2022-11-21 22:02:01 -0500

    switch statement configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Home from "./components/Home";
+import About from "./components/About"
+
 
 function App() {
+  let component
+  switch (window.location.pathname) {
+    case '/':
+      component = <Home />
+      break;
+    case '/about':
+      component = <About />
+      break;
+    case '/pricing':
+      component = <Pricing />
+      break;
+    default:
+      component = <Home />
+      break;
+  }
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      {component}
+    </>
   );
 }
 
