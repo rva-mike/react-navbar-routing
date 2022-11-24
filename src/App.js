c1c3796798008513353563f356fff48918605371
rva-mike <mike.levy.rva@gmail.com>
2022-11-24 11:36:38 -0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,31 +2,24 @@ import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
 import Home from "./components/Home";
 import About from "./components/About"
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let component
-  switch (window.location.pathname) {
-    case '/':
-      component = <Home />
-      break;
-    case '/about':
-      component = <About />
-      break;
-    case '/pricing':
-      component = <Pricing />
-      break;
-    default:
-      component = <Home />
-      break;
-  }
   return (
     <>
       <Navbar />
-      <div className="container">{component}</div>
-      
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+
     </>
-  );
+
+  )
+
 }
 
 export default App;
